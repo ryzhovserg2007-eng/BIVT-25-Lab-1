194d6631161822abae04f661f4c24d8faf06526b
ryzhovserg2007-eng <ryzhov.serg.2007@gmail.com>
2025-09-05 12:17:18 +0300

    Update Blue.cs

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -7,7 +7,7 @@
             bool answer = false;
 
             // code here
-
+            break;
             // end
 
             return answer;
@@ -84,4 +84,5 @@
             return answer;
         }
     }
+
 }
